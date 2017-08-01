6e7c7a129e0c0aabc4ef870048970da44ac8d4bd
lucasbrambrink <lucas@ycharts.com>
2017-08-01 13:35:04 -0400

    adds settings query

--- earth/api/v0/earth/views.py
+++ earth/api/v0/earth/views.py
@@ -1,19 +1,47 @@
 import random
 
+from django.db.models import Q
+
 from rest_framework.permissions import AllowAny
 from rest_framework import generics
 from rest_framework import status
 from rest_framework.response import Response
 from .serializers import EarthImageSerializer
-from api.models import EarthImage
+from api.models import EarthImage, QuerySetting
 
 
 class EarthImageView(generics.RetrieveAPIView):
     permission_classes = (AllowAny,)
     serializers = EarthImageSerializer
 
-    def get(self, request, *args, **kwargs):
-        all_ids = EarthImage.objects.values_list('id', flat=True)
-        random_obj = EarthImage.objects.get(id=random.choice(all_ids))
-        return Response(EarthImageSerializer(random_obj).data,
+    def get_random_object(self, all_ids=None):
+        all_ids = all_ids or EarthImage.objects.values_list('id', flat=True)
+        return EarthImage.objects.get(id=random.choice(all_ids))
+
+    def get_object_via_settings(self, settings_uid):
+        query_ids = None
+        try:
+            setting = QuerySetting.objects\
+                .exclude(query_keywords_title='')\
+                .get(url_identifier=settings_uid)
+        except QuerySetting.DoesNotExist:
+            pass
+        else:
+            query_kwargs = [Q(title__icontains=kw.strip())
+                            for kw in setting.query_keywords_title.split(',')]
+            query = query_kwargs.pop()
+            for item in query_kwargs:
+                query |= item
+            query_ids = EarthImage.objects.filter(query).values_list('id', flat=True)
+
+            if not query_ids.count():
+                query_ids = None
+
+        return self.get_random_object(query_ids)
+
+    def get(self, request, settings_uid=None, *args, **kwargs):
+        obj = self.get_random_object() if settings_uid is None else \
+            self.get_object_via_settings(settings_uid)
+
+        return Response(EarthImageSerializer(obj).data,
                         status=status.HTTP_200_OK)
