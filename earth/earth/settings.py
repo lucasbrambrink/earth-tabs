e763b4ca55151f4587cdc2fadaf87e3be0a00807
Lucas Brambrink <lucasbrambrink@Lucass-MacBook-Pro.local>
2017-08-02 00:27:11 -0400

    fix static files

--- earth/earth/settings.py
+++ earth/earth/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'earth')
+STATIC_ROOT = os.path.join(BASE_DIR, 'root')
 
 SHELL_PLUS_PRE_IMPORTS = (
     ('api.data_source', ('EarthScraper'))
