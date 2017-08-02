4a320e9eb9ffa1cd8a90cc9d296b9352795c2e08
Lucas Brambrink <lucasbrambrink@Lucass-MacBook-Pro.local>
2017-08-02 00:16:52 -0400

    adds static root

--- earth/earth/settings.py
+++ earth/earth/settings.py
@@ -123,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'earth')
 
 SHELL_PLUS_PRE_IMPORTS = (
     ('api.data_source', ('EarthScraper'))
