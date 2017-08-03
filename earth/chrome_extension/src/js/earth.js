0b2e43b361feef901585f7c72e6f01853e391319
lucasbrambrink <lucas@ycharts.com>
2017-08-03 17:00:02 -0400

    builds out chromeextension

--- earth/chrome_extension/src/js/earth.js
+++ earth/chrome_extension/src/js/earth.js
@@ -1,107 +1,111 @@
 // Get Options
 
-chrome.topSites.get(top_sites);
+// chrome.topSites.get(top_sites);
 
-var random_sub = [];
+// var random_sub = [];
 
-function restore_options() {
+// function restore_options() {
+//
+//     chrome.storage.sync.get({
+//         show_title: true,
+//         show_sites: false,
+//         subreddits: true
+//     }, function (items) {
+//
+//
+//         if (items.show_title == false) {
+//             $('.info').css('display', 'none');
+//         }
+//         if (items.show_sites == true) {
+//             $('.top_sites').css('display', 'inherit');
+//         }
+//
+//         subreddit = items.subreddits;
+//         x = 0;
+//         for (var index in subreddit) {
+//
+//             if (subreddit[index] == 1) {
+//                 //    console.log(index);
+//                 random_sub[x] = index;
+//                 x++;
+//             }
+//
+//         }
+//
+//         subreddit = random_sub[Math.floor(Math.random() * random_sub.length)];
+//         if (subreddit == null) {
+//             subreddit = 'earthporn';
+//         }
+//
+//         new_photo(subreddit);
+//
+//     });
+//
+// }
+var API_URL = 'https://earth-pics.tk/api/v0/earth/get';
 
-    chrome.storage.sync.get({
-        show_title: true,
-        show_sites: false,
-        subreddits: true
-    }, function (items) {
-
-
-        if (items.show_title == false) {
-            $('.info').css('display', 'none');
-        }
-        if (items.show_sites == true) {
-            $('.top_sites').css('display', 'inherit');
-        }
-
-        subreddit = items.subreddits;
-        x = 0;
-        for (var index in subreddit) {
-
-            if (subreddit[index] == 1) {
-                //    console.log(index);
-                random_sub[x] = index;
-                x++;
-            }
-
-        }
-
-        subreddit = random_sub[Math.floor(Math.random() * random_sub.length)];
-        if (subreddit == null) {
-            subreddit = 'earthporn';
-        }
-
-        new_photo(subreddit);
-
-    });
-
-}
+var settings_identifier = null;
+chrome.storage.sync.get("settings_uid", function (item) {
+    settings_identifier = item;
+});
 
 // Attempt to get a photo from local storage
-
-
-var photo = localStorage.getItem('photo');
-if (photo == null) {
-
-    // If a photo doesn't exist, retrieve one from server and set it as localStorage item
-    new_photo();
-
-    var photo = localStorage.getItem('photo');
-
-}
-
-// Parse the image data
-the_image = JSON.parse(photo);
-
-$('.background').css("background-image", "url('" + the_image.url + "')");
-
-$('.title').attr("href", "http://reddit.com" + the_image.permalink);
-$('.title').html(the_image.title);
-$('.author').html("By " + the_image.author);
-$('.ups').html(the_image.ups);
-$('.info').css('opacity', '0.6');
-
-
-// cache photo as previous
-var lastPhoto = localStorage.getItem('lastPhoto');
-if (lastPhoto == null) {
-    $('.last-title').remove();
+var currentImage = localStorage.getItem('currentImage');
+if (currentImage === null) {
+    getNewImage(settings_identifier);
 } else {
-    $('.last-title').attr('href', "http://reddit.com" + lastPhoto);
-}
-localStorage.setItem('lastPhoto', the_image.permalink);
-
-function new_photo(subreddit) {
-
-    $.getJSON("http://tab.pics/api/" + subreddit, function (data) {
-        data.data_uri = null;
-        var dataToStore = JSON.stringify(data);
-        localStorage.removeItem('photo');
-        localStorage.setItem('photo', dataToStore);
-
-    }).fail(function () {
-        trackError('Image Request Failed');
-    });
-
-}
-
-function top_sites(data) {
-
-    $.each(data, function (index, value) {
-
-        $('.top_sites').append("<div class='site_container'><div class='site'><a href='" + value.url + "'>" + value.title.substring(0, 25) + "..<div class='url'>" + value.url + "</div></div></a></div>");
-        index++;
-        if (index == 5)
-            return false;
-    });
-
+    setImage(JSON.parse(currentImage));
 }
 
 
-document.addEventListener('DOMContentLoaded', restore_options);
+function setImage(imageData) {
+    $('.background').css("background-image", "url('" + imageData.preferred_image_url + "')");
+
+    $('.title')
+        .attr("href", "http://reddit.com" + imageData.permalink)
+        .html(imageData.title);
+    $('.author').html("By " + imageData.author);
+    $('.ups').html(imageData.ups);
+    $('.info').css('opacity', '0.6');
+
+}
+
+// // cache photo as previous
+// var lastPhoto = localStorage.getItem('lastPhoto');
+// if (lastPhoto === null) {
+//     $('.last-title').remove();
+// } else {
+//     $('.last-title').attr('href', "http://reddit.com" + lastPhoto);
+// }
+// localStorage.setItem('lastPhoto', the_image.permalink);
+
+function getNewImage(settings_uid) {
+    var url = API_URL;
+    if (settings_uid !== null) {
+        url += '/' + settings_uid;
+    }
+
+    $.getJSON(url)
+        .success(function(resp) {
+            newImage = resp;
+            localStorage.setItem('currentImage', JSON.stringify(newImage));
+            setImage(newImage);
+        }).fail(function () {
+            console.log('Image Request Failed');
+        });
+}
+
+// function top_sites(data) {
+//
+//     $.each(data, function (index, value) {
+//
+//         $('.top_sites').append("<div class='site_container'><div class='site'><a href='" + value.url + "'>" + value.title.substring(0, 25) + "..<div class='url'>" + value.url + "</div></div></a></div>");
+//         index++;
+//         if (index == 5)
+//             return false;
+//     });
+//
+// }
+
+
+// document.addEventListener('DOMContentLoaded', restore_options);
