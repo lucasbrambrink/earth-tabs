c6295b308d28a40b8730c2dc7baecb0a98e5fe3f
Lucas Brambrink <lucasbrambrink@Lucass-MacBook-Pro.local>
2018-01-14 02:35:26 -0500

    adds location.csv

--- earth/api/utils/scraping.py
+++ earth/api/utils/scraping.py
@@ -191,6 +191,20 @@ class StateParks(object):
         'https://en.wikipedia.org/wiki/List_of_Wisconsin_state_parks',
         'https://en.wikipedia.org/wiki/List_of_Wyoming_state_parks'
     ]
+    STATES = {
+         'Alabama','Alaska','Arizona','Arkansas','California','Colorado',
+         'Connecticut','Delaware','Florida','Georgia','Hawaii','Idaho',
+         'Illinois','Indiana','Iowa','Kansas','Kentucky','Louisiana',
+         'Maine' 'Maryland','Massachusetts','Michigan','Minnesota',
+         'Mississippi', 'Missouri','Montana','Nebraska','Nevada',
+         'New Hampshire','New Jersey','New Mexico','New York',
+         'North Carolina','North Dakota','Ohio',
+         'Oklahoma','Oregon','Pennsylvania','Rhode Island',
+         'South  Carolina','South Dakota','Tennessee','Texas','Utah',
+         'Vermont','Virginia','Washington','West Virginia',
+         'Wisconsin','Wyoming'
+    }
+
     def get_all_urls(self):
         content = ScrapingMixin.get(self.link, as_json=False)
         soup = BeautifulSoup(content)
